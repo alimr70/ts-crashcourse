5ac1a24f6a541c61e1e039bc0e3c35c20461b2ab
unknown <ali.maher.mohammed@gmail.com>
2022-09-22 20:08:34 +0200

    Finished

--- dist/index.js
+++ dist/index.js
@@ -28,7 +28,7 @@ var Direction1;
     Direction1[Direction1["Left"] = 3] = "Left";
     Direction1[Direction1["Right"] = 4] = "Right";
 })(Direction1 || (Direction1 = {}));
-console.log(Direction1.Left);
+// console.log(Direction1.Left)
 var Direction2;
 (function (Direction2) {
     Direction2["Up"] = "Up";
@@ -36,7 +36,6 @@ var Direction2;
     Direction2["Left"] = "Left";
     Direction2["Right"] = "Right";
 })(Direction2 || (Direction2 = {}));
-console.log(Direction2.Left);
 const user = {
     id: 1,
     name: 'Loli'
@@ -45,3 +44,46 @@ const user = {
 let cid = 1;
 // let customerId = <number>cid
 let customerId = cid;
+// Functions
+function addNum(x, y) {
+    return x + y;
+}
+// console.log(addNum(4, 2))
+// Void
+function log(message) {
+    console.log(message);
+}
+const user1 = {
+    id: 1,
+    name: 'Loli'
+};
+const add = (x, y) => x + y;
+const sub = (x, y) => x - y;
+// Classes
+class Person {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+    }
+    register() {
+        return `${this.name} is now registered`;
+    }
+}
+const ali = new Person(1, 'ali');
+// console.log(ali.register())
+// Subclasses
+class Employee extends Person {
+    constructor(id, name, position) {
+        super(id, name);
+        this.postition = position;
+    }
+}
+// const emp = new Employee(3, 'Moazaf', 'Developer')
+// console.log(emp.register())
+// Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArr = getArray([1, 2, 3, 4]);
+let strArr = getArray(['ali', 'maher', 'mohammed']);
+// numArr.push(5)
--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ enum Direction1 {
   Right
 }
 
-console.log(Direction1.Left)
+// console.log(Direction1.Left)
 
 enum Direction2 {
   Up = 'Up',
@@ -42,7 +42,7 @@ enum Direction2 {
   Right = 'Right'
 }
 
-console.log(Direction2.Left)
+// console.log(Direction2.Left)
 
 // Objects
 type User = {
@@ -59,3 +59,79 @@ const user: User = {
 let cid: any = 1
 // let customerId = <number>cid
 let customerId = cid as number
+
+// Functions
+function addNum (x: number, y: number): number {
+  return x + y
+}
+
+// console.log(addNum(4, 2))
+
+// Void
+function log (message: string | number): void {
+  console.log(message)
+}
+
+// Interfaces
+interface UserInterface {
+  readonly id: number,
+  name: string,
+  age?: number
+}
+
+const user1: UserInterface = {
+  id: 1,
+  name: 'Loli'
+}
+
+interface MathFunc {
+  (x: number, y: number): number
+}
+
+const add: MathFunc = (x: number, y:number): number => x + y
+const sub: MathFunc = (x: number, y:number): number => x - y
+
+interface PersonInterface {
+  id: number,
+  name: string,
+  register(): string
+}
+
+// Classes
+class Person implements PersonInterface {
+  id: number
+  name: string
+  constructor(id: number, name: string){
+    this.id = id
+    this.name = name
+  }
+  register(){
+    return `${this.name} is now registered`
+  }
+}
+
+const ali = new Person(1, 'ali')
+// console.log(ali.register())
+
+// Subclasses
+class Employee extends Person {
+  postition: string
+
+  constructor(id: number, name: string, position: string){
+    super(id, name)
+    this.postition = position
+  }
+}
+
+// const emp = new Employee(3, 'Moazaf', 'Developer')
+// console.log(emp.register())
+
+// Generics
+function getArray<T>(items: T[]): T[] {
+  return new Array().concat(items)
+}
+
+let numArr = getArray<number>([1,2,3,4])
+let strArr = getArray<string>(['ali', 'maher', 'mohammed'])
+
+// numArr.push(5)
